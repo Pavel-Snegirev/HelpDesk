360d8e7a7bbb803e06542ee673031b3224cc751d
snegirev_pavel86@bk.ru <snegirev_pavel86@bk.ru>
2019-10-23 07:21:47 +0300

    Добавьте файлы проекта.

---
+++ Help-Desk/General.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Help_Desk
+{
+    partial class General
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(General));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.bunifuDragControl1 = new ns1.BunifuDragControl(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(48)))), ((int)(((byte)(72)))));
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.pictureBox8);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Name = "panel1";
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            // 
+            // pictureBox8
+            // 
+            resources.ApplyResources(this.pictureBox8, "pictureBox8");
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.MouseLeave += new System.EventHandler(this.PictureBox7_MouseLeave_1);
+            this.pictureBox7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox7_MouseMove_1);
+            // 
+            // pictureBox6
+            // 
+            resources.ApplyResources(this.pictureBox6, "pictureBox6");
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseLeave += new System.EventHandler(this.PictureBox6_MouseLeave_1);
+            this.pictureBox6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox6_MouseMove_1);
+            // 
+            // pictureBox5
+            // 
+            resources.ApplyResources(this.pictureBox5, "pictureBox5");
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseLeave += new System.EventHandler(this.PictureBox5_MouseLeave_1);
+            this.pictureBox5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox5_MouseMove_1);
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseLeave += new System.EventHandler(this.PictureBox3_MouseLeave_1);
+            this.pictureBox3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox3_MouseMove_1);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.PictureBox2_MouseLeave_1);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox2_MouseMove_1);
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2")});
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(172)))), ((int)(((byte)(246)))));
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(121)))), ((int)(((byte)(172)))), ((int)(((byte)(246)))));
+            this.label1.Name = "label1";
+            // 
+            // panel3
+            // 
+            resources.ApplyResources(this.panel3, "panel3");
+            this.panel3.Controls.Add(this.map);
+            this.panel3.Name = "panel3";
+            // 
+            // map
+            // 
+            resources.ApplyResources(this.map, "map");
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemmory = 5;
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Zoom = 0D;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // General
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "General";
+            this.Load += new System.EventHandler(this.General_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private ns1.BunifuDragControl bunifuDragControl1;
+    }
+}
---
+++ Help-Desk/General.cs
@@ -0,0 +1,144 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Help_Desk
+{
+    public partial class General : Form
+    {
+        public General()
+        {
+            InitializeComponent();
+        }
+        private void General_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+
+        //*********************************начало секции hover: кнопок управления переход открытие форм проверка многократности*************************************//
+
+        private void PictureBox2_MouseLeave_1(object sender, EventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_cell_phone_50px_1.png");
+        }
+
+        private void PictureBox2_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_cell_phone_50px_2.png");
+        }
+
+        private void PictureBox3_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            pictureBox3.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_checked_50px_3.png");
+        }
+
+        private void PictureBox3_MouseLeave_1(object sender, EventArgs e)
+        {
+            pictureBox3.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_checked_50px_4.png");
+        }
+
+        private void PictureBox5_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            pictureBox5.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_combo_chart_50px.png");
+        }
+
+        private void PictureBox5_MouseLeave_1(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_combo_chart_50px_1.png");
+        }
+
+        private void PictureBox6_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            pictureBox6.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_geography_50px.png");
+        }
+
+        private void PictureBox6_MouseLeave_1(object sender, EventArgs e)
+        {
+            pictureBox6.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_geography_50px_1.png");
+        }
+
+        private void PictureBox7_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            pictureBox7.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_email_50px_3.png");
+        }
+
+        private void PictureBox7_MouseLeave_1(object sender, EventArgs e)
+        {
+            pictureBox7.Image = Image.FromFile(@"G:\Проекты C#\Help-Desk\Help-Desk\bin\Debug\img\icons8_email_50px_2.png");
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+            //formDeskGPS.MdiParent = this;
+
+
+            //if (formDeskGPS == null) //  проверка на многократное открытие формы
+            //{
+                                
+            //    formDeskGPS.FormClosed += (x, y) => { formDeskGPS = null; }; //для избежания проблем с повторным открытием после закрытия
+            //}
+            //formDeskGPS.Show();
+           
+        }
+
+
+
+
+        //*************************************конец секции hover : кнопок управления***********************************************************************//
+
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void PictureBox9_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            map.Visible = true;
+            
+
+            map.DragButton = MouseButtons.Left;
+
+            map.MapProvider = GMapProviders.GoogleMap;
+            double lat = 56.8456729; //широта
+            double longt = 35.8955904; //долгота
+            map.Position = new GMap.NET.PointLatLng(lat, longt);
+            map.MinZoom = 5;
+            map.MaxZoom = 200;
+            map.Zoom = 15;
+            PointLatLng point = new PointLatLng(lat, longt);
+            GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.blue_dot);
+            GMapOverlay markers = new GMapOverlay("markers");
+            markers.Markers.Add(marker);
+            map.Overlays.Add(markers);
+
+
+        }
+
+        
+    }
+}
